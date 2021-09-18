ec916986a31c560a49cf7e913e6cf4099e237a1b
ptaushanov <pepi15032000@gmail.com>
2021-09-18 16:05:43 +0300

    refactor: chanded ramda filter to vanilla filter

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,13 @@
 import "../scss/app.scss";
-const R = require("ramda");
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
   const ul = document.querySelector("ul");
   const numbersArray = [1, 3, 4, 5, 6, 7];
   const isEven = (n) => n % 2 === 0;
-  const filteredArray = R.filter(isEven, numbersArray);
+  const filteredArray = numbersArray.filter(isEven);
+  
+  const appendListItem = number => ul.innerHTML += `<li> ${number} </li>`
+  filteredArray.forEach(appendListItem)
+  
 });
